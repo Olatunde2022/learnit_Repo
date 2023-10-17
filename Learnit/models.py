5ef37a32993fde858573beb5e344969960b00ed8
unknown <ojekab2580@gmail.com>
2023-10-16 17:07:20 -0700

    commit

--- Learnit/models.py
+++ Learnit/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Review(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    Name = models.CharField(max_length=40)
+    Body = models.CharField(max_length=10000,null=False)
+   
--- student/views.py
+++ student/views.py
@@ -80,82 +80,82 @@ def eachstudent(request,id):
     return render(request,'student/for_dashboard.html', context)
     
 
-@login_required(login_url='studentlogin')
-@user_passes_test(is_student)
-def student_exam_view(request):
-    courses=QMODEL.Course.objects.all()
-    return render(request,'student/student_exam.html',{'courses':courses})
+# @login_required(login_url='studentlogin')
+# @user_passes_test(is_student)
+# def student_exam_view(request):
+#     courses=QMODEL.Course.objects.all()
+#     return render(request,'student/student_exam.html',{'courses':courses})
 
-@login_required(login_url='studentlogin')
-@user_passes_test(is_student)
-def take_exam_view(request,pk):
-    course=QMODEL.Course.objects.get(id=pk)
-    total_questions=QMODEL.Question.objects.all().filter(course=course).count()
-    questions=QMODEL.Question.objects.all().filter(course=course)
-    total_marks=0
-    for q in questions:
-        total_marks=total_marks + q.marks
+# @login_required(login_url='studentlogin')
+# @user_passes_test(is_student)
+# def take_exam_view(request,pk):
+#     course=QMODEL.Course.objects.get(id=pk)
+#     total_questions=QMODEL.Question.objects.all().filter(course=course).count()
+#     questions=QMODEL.Question.objects.all().filter(course=course)
+#     total_marks=0
+#     for q in questions:
+#         total_marks=total_marks + q.marks
     
-    return render(request,'student/take_exam.html',{'course':course,'total_questions':total_questions,'total_marks':total_marks})
+#     return render(request,'student/take_exam.html',{'course':course,'total_questions':total_questions,'total_marks':total_marks})
 
-@login_required(login_url='studentlogin')
-@user_passes_test(is_student)
-def start_exam_view(request,pk):
-    course=QMODEL.Course.objects.get(id=pk)
-    questions=QMODEL.Question.objects.all().filter(course=course)
-    if request.method=='POST':
-        pass
-    response= render(request,'student/start_exam.html',{'course':course,'questions':questions})
-    response.set_cookie('course_id',course.id)
-    return response
+# @login_required(login_url='studentlogin')
+# @user_passes_test(is_student)
+# def start_exam_view(request,pk):
+#     course=QMODEL.Course.objects.get(id=pk)
+#     questions=QMODEL.Question.objects.all().filter(course=course)
+#     if request.method=='POST':
+#         pass
+#     response= render(request,'student/start_exam.html',{'course':course,'questions':questions})
+#     response.set_cookie('course_id',course.id)
+#     return response
 
 
-@login_required(login_url='studentlogin')
-@user_passes_test(is_student)
-def calculate_marks_view(request):
-    if request.COOKIES.get('course_id') is not None:
-        course_id = request.COOKIES.get('course_id')
-        course=QMODEL.Course.objects.get(id=course_id)
+# @login_required(login_url='studentlogin')
+# @user_passes_test(is_student)
+# def calculate_marks_view(request):
+#     if request.COOKIES.get('course_id') is not None:
+#         course_id = request.COOKIES.get('course_id')
+#         course=QMODEL.Course.objects.get(id=course_id)
         
-        total_marks=0
-        questions=QMODEL.Question.objects.all().filter(course=course)
-        for i in range(len(questions)):
+#         total_marks=0
+#         questions=QMODEL.Question.objects.all().filter(course=course)
+#         for i in range(len(questions)):
             
-            selected_ans = request.COOKIES.get(str(i+1))
-            actual_answer = questions[i].answer
-            if selected_ans == actual_answer:
-                total_marks = total_marks + questions[i].marks
-        student = models.Student.objects.get(user_id=request.user.id)
-        result = QMODEL.Result()
-        result.marks=total_marks
-        result.exam=course
-        result.student=student
-        result.save()
+#             selected_ans = request.COOKIES.get(str(i+1))
+#             actual_answer = questions[i].answer
+#             if selected_ans == actual_answer:
+#                 total_marks = total_marks + questions[i].marks
+#         student = models.Student.objects.get(user_id=request.user.id)
+#         result = QMODEL.Result()
+#         result.marks=total_marks
+#         result.exam=course
+#         result.student=student
+#         result.save()
 
-        return HttpResponseRedirect('view-result')
+#         return HttpResponseRedirect('view-result')
 
 
 
-@login_required(login_url='studentlogin')
-@user_passes_test(is_student)
-def view_result_view(request):
-    courses=QMODEL.Course.objects.all()
-    return render(request,'student/view_result.html',{'courses':courses})
+# @login_required(login_url='studentlogin')
+# @user_passes_test(is_student)
+# def view_result_view(request):
+#     courses=QMODEL.Course.objects.all()
+#     return render(request,'student/view_result.html',{'courses':courses})
     
 
-@login_required(login_url='studentlogin')
-@user_passes_test(is_student)
-def check_marks_view(request,pk):
-    course=QMODEL.Course.objects.get(id=pk)
-    student = models.Student.objects.get(user_id=request.user.id)
-    results= QMODEL.Result.objects.all().filter(exam=course).filter(student=student)
-    return render(request,'student/check_marks.html',{'results':results})
+# @login_required(login_url='studentlogin')
+# @user_passes_test(is_student)
+# def check_marks_view(request,pk):
+#     course=QMODEL.Course.objects.get(id=pk)
+#     student = models.Student.objects.get(user_id=request.user.id)
+#     results= QMODEL.Result.objects.all().filter(exam=course).filter(student=student)
+#     return render(request,'student/check_marks.html',{'results':results})
 
-@login_required(login_url='studentlogin')
-@user_passes_test(is_student)
-def student_marks_view(request):
-    courses=QMODEL.Course.objects.all()
-    return render(request,'student/student_marks.html',{'courses':courses})
+# @login_required(login_url='studentlogin')
+# @user_passes_test(is_student)
+# def student_marks_view(request):
+#     courses=QMODEL.Course.objects.all()
+#     return render(request,'student/student_marks.html',{'courses':courses})
 
 
 
